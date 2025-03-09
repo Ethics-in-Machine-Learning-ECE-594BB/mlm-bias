9baa50538add5add0ae5928a215e5d5a5aecca4f
Ethan Epp <eepp@ucsb.edu>
2025-03-08 22:08:30 -0800

    usage

--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mlm-bias',
+    name='mlm-bias-fork',
     version='0.1.6',
-    author='Rahul Zalkikar',
-    author_email='rayzck9@gmail.com',
+    author='Ethan Epp',
+    author_email='eepp@ucsb.edu',
     description='Bias Evaluation Methods for Masked Language Models implemented in PyTorch',
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/zalkikar/mlm-bias',
-    project_urls={
-        'Bug Tracker': 'https://github.com/zalkikar/mlm-bias/issues',
-    },
+    url='https://github.com/EthanEpp/mlm-bias',
     python_requires=">=3.8, <3.12",
     packages=find_packages(),
     install_requires=[
